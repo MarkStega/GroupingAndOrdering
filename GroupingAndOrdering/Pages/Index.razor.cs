4800860317e1e28c58ebc7e6232c15646129a561
Simon Ziegler <simon@dioptra.tech>
2021-01-25 21:19:03 +0000

    Update

---
+++ GroupingAndOrdering/Pages/Index.razor.cs
@@ -0,0 +1,67 @@
+﻿using GroupingAndOrdering.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupingAndOrdering.Pages
+{
+    public partial class Index
+    {
+        private class Person
+        {
+            public Guid UniqueIdentifier { get; set; }
+            public string Salutation { get; set; }
+            public string GivenName { get; set; }
+            public string FamilyName { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Salutation} {GivenName} *{FamilyName}*";
+            }
+        }
+
+        private Person[] People =
+        {
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Prof", GivenName = "Marie", FamilyName = "Curie" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Prof", GivenName = "Albert", FamilyName = "Einstein" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Prof", GivenName = "Andrew", FamilyName = "Huxley" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Bob", FamilyName = "Dylan" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Barack", FamilyName = "Obama" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Ms", GivenName = "Nadine", FamilyName = "Gordimer" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Muhammad", FamilyName = "Yunus" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "RtHon", GivenName = "Lord", FamilyName = "Rayleigh" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Ms", GivenName = "Grazia", FamilyName = "Deledda" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Jean-Paul", FamilyName = "Sartre" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Prof", GivenName = "Esther", FamilyName = "Duflo" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Prof", GivenName = "Yoshinori", FamilyName = "Ohsumi" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Prof", GivenName = "Robert", FamilyName = "Merton" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Prof", GivenName = "Barbara", FamilyName = "McClintock" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Boris", FamilyName = "Pasternak" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Willy", FamilyName = "Brandt" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Isaac", FamilyName = "Bashevis Singer" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Ms", GivenName = "Olga", FamilyName = "Tokarczuk" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "Günter", FamilyName = "Grass" },
+            new Person() { UniqueIdentifier=Guid.NewGuid(), Salutation = "Mr", GivenName = "John", FamilyName = "Hume" },
+        };
+
+        private readonly string[] Salutations = { "Dr", "Mr", "Ms", "Prof", "Lord", "Mrs" };
+
+        private bool GroupData { get; set; }
+        private bool GroupDescending { get; set; }
+        private bool ItemsDescending { get; set; }
+        private bool ShowEmptyGroups { get; set; }
+
+
+        private IOrderedEnumerable<Person> OrderPeople(IEnumerable<Person> people, Direction direction) => direction == Direction.Ascending ? people.OrderBy(x => x.FamilyName) : people.OrderByDescending(x => x.FamilyName);
+
+        private IEnumerable<IGrouping<string, Person>> GroupMethod(IEnumerable<Person> people)
+        {
+            var result = people.GroupBy(x => x.Salutation);
+
+            return result;
+        }
+
+        private IOrderedEnumerable<string> OrderSalutations(IEnumerable<string> salutations, Direction direction) => direction == Direction.Ascending ? salutations.OrderBy(x => x) : salutations.OrderByDescending(x => x);
+
+    }
+}
---
+++ GroupingAndOrdering/Shared/NotQuiteGrid.razor.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupingAndOrdering.Shared
+{
+    public enum Direction { Ascending, Descending }
+
+    public partial class NotQuiteGrid<TItem, TGroupKey>
+    {
+
+        [Parameter] public IEnumerable<TItem> Items { get; set; }
+
+        [Parameter] public Func<IEnumerable<TItem>, Direction, IOrderedEnumerable<TItem>> OrderItems { get; set; }
+
+        [Parameter] public Direction ItemsDirection { get; set; } = Direction.Ascending;
+
+
+        [Parameter] public Func<IEnumerable<TItem>, IEnumerable<IGrouping<TGroupKey, TItem>>> GroupMethod { get; set; }
+
+        [Parameter] public Func<IEnumerable<TGroupKey>, Direction, IOrderedEnumerable<TGroupKey>> OrderGroupKeys { get; set; }
+
+        [Parameter] public Direction GroupKeysDirection { get; set; } = Direction.Ascending;
+
+        [Parameter] public IEnumerable<TGroupKey> GroupCategories { get; set; }
+
+
+
+        private List<TItem> UngroupedItems { get; set; }
+
+        private Dictionary<TGroupKey, List<TItem>> GroupDict { get; set; }
+
+        private List<TGroupKey> GroupKeys { get; set; }
+
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            UngroupedItems = null;
+            GroupDict = null;
+            GroupKeys = null;
+
+            if (GroupMethod == null)
+            {
+                UngroupedItems = (OrderItems == null ? Items : OrderItems(Items, ItemsDirection)).ToList();
+            }
+            else
+            {
+                if (OrderGroupKeys == null)
+                {
+                    GroupDict = GroupMethod(Items).ToDictionary(x => x.Key, x => x.ToList());
+                }
+                else
+                {
+                    GroupDict = GroupMethod(Items).ToDictionary(x => x.Key, x => OrderItems(x, ItemsDirection).ToList());
+                }
+
+                if (GroupCategories != null)
+                {
+                    var missingKeys = GroupCategories.Except(GroupDict.Keys);
+
+                    foreach (var key in missingKeys)
+                    {
+                        GroupDict.Add(key, new());
+                    }
+                }
+
+                IEnumerable<TGroupKey> ok = OrderGroupKeys == null ? GroupDict.Keys : OrderGroupKeys(GroupDict.Keys, GroupKeysDirection);
+
+                GroupKeys = ok.ToList();
+            }
+        }
+    }
+}
